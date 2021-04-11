b31502a7f70ca69a0cf88952ba3d871698909262
Mohammed Samir <ms.2052001@gmail.com>
2021-04-11 04:19:21 +0200

    add eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {},
+  overrides: [
+    {
+      files: ["./src/**/*.js"],
+      env: {
+        browser: true,
+        es2021: true
+      }
+    }
+  ]
+};
