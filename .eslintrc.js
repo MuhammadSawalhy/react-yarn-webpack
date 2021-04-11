ee629c18c33b93a6a201f99784278ec4996bacee
Mohammed Samir <ms.2052001@gmail.com>
2021-04-11 15:01:04 +0200

    update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,23 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
-    sourceType: "module"
-  },
-  plugins: ["react"],
+  extends: ["eslint:recommended"],
   rules: {},
   overrides: [
     {
       files: ["./src/**/*.js"],
+      extends: ["eslint:recommended", "plugin:react/recommended"],
+      plugins: ["react"],
       env: {
         browser: true,
         es2021: true
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: "module"
       }
     }
   ]
