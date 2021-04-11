909a5c6633a937abb003431c0496584287e6e83c
Mohammed Samir <ms.2052001@gmail.com>
2021-04-11 04:20:26 +0200

    use husky pre-commit hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
-const micromatch = require('micromatch');
-const prettier = require('prettier');
+const micromatch = require("micromatch");
+const prettier = require("prettier");
 
 const addQuotes = (a) => `'${a}'`;
 
@@ -14,17 +14,23 @@ function prettierCmds() {
     allStagedFiles,
     prettierSupportedExtensions.map((extension) => `**/*${extension}`)
   );
-  return files.length ? [`prettier --write ${files.map(addQuotes).join(' ')}`] : [];
+  return files.length
+    ? [`prettier --write ${files.map(addQuotes).join(" ")}`]
+    : [];
 }
 
 function eslintCmds() {
-  const files = micromatch(allStagedFiles, '**/*.js');
-  return files.length ? [`eslint --cache --fix ${files.map(addQuotes).join(' ')}`] : [];
+  const files = micromatch(allStagedFiles, "**/*.js");
+  return files.length
+    ? [`eslint --cache --fix ${files.map(addQuotes).join(" ")}`]
+    : [];
 }
 
 function syncpackCmds() {
-  const files = micromatch(allStagedFiles, '**/package.json');
-  return files.length ? [`syncpack format --source '{${files.join(',')}}'`] : [];
+  const files = micromatch(allStagedFiles, "**/package.json");
+  return files.length
+    ? [`syncpack format --source '{${files.join(",")}}'`]
+    : [];
 }
 
 module.exports = (files) => {
