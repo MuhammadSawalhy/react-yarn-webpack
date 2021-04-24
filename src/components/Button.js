264d7e796ff19e0d794167f492e1e880a9b4d2e6
Mohammed Samir <ms.2052001@gmail.com>
2021-04-24 12:54:14 +0200

    add the Button story

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,13 @@ import "./Button.css";
 import React from "react";
 
 // eslint-disable-next-line react/prop-types
-const Button = ({ children, ...others }) => {
-  return <button {...others}>{children}</button>;
+const Button = ({ children, background, size, onClick }) => {
+  background ??= "rgb(113, 39, 211)";
+  return (
+    <button className={size} style={{ background }} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
